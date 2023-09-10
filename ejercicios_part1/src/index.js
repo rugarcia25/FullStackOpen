abbfc8627ce6e82bc5fe97dc82e85cacaa941b3c
Ruben <rgarciarios95@gmail.com>
2023-09-10 16:45:55 +0200

    paso 2

--- ejercicios_part1/src/index.js
+++ ejercicios_part1/src/index.js
@@ -10,16 +10,24 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  const num = 1;
+  const part1 = "Fundamentals of React";
+  const part2 = "Using props to pass data";
+  const part3 = "State of a component";
+  return (
+    <div>
+      <Part num={num} part1={part1} />
+      <Part num={num + 1} part2={part2} />
+      <Part num={num + 2} part3={part3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
   return (
     <div>
       <p>
-        Parte 1 <b>{props.part1}</b>
-      </p>
-      <p>
-        Parte 2 <b>{props.part2}</b>
-      </p>
-      <p>
-        Parte 3 <b>{props.part3}</b>
+        <h3>Parte {props.num}</h3> {props.part1} {props.part2} {props.part3}
       </p>
     </div>
   );
@@ -37,17 +45,14 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  const part2 = "Using props to pass data";
   const exercises2 = 7;
-  const part3 = "State of a component";
   const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
